dfee7a38130454ad3bb7761dd19824b761e95801
Jared Dalessandro <jareddalessandro1@gmail.com>
2024-10-02 22:48:05 -0700

    first commit

---
+++ InventorySystem/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace InventorySystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            dataGridView2 = new DataGridView();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            textBox2 = new TextBox();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(37, 149);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(579, 275);
+            dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Location = new Point(750, 149);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.Size = new Size(604, 275);
+            dataGridView2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(37, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(320, 30);
+            label1.TabIndex = 2;
+            label1.Text = "Inventory Management System";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(37, 115);
+            label2.Name = "label2";
+            label2.Size = new Size(48, 21);
+            label2.TabIndex = 3;
+            label2.Text = "Parts";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(750, 115);
+            label3.Name = "label3";
+            label3.Size = new Size(77, 21);
+            label3.TabIndex = 4;
+            label3.Text = "Products";
+            label3.Click += label3_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
+            textBox1.Location = new Point(1190, 113);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(164, 23);
+            textBox1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(1115, 113);
+            button1.Name = "button1";
+            button1.Size = new Size(60, 23);
+            button1.TabIndex = 6;
+            button1.Text = "Search";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(376, 111);
+            button2.Name = "button2";
+            button2.Size = new Size(60, 23);
+            button2.TabIndex = 7;
+            button2.Text = "Search";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            textBox2.BorderStyle = BorderStyle.FixedSingle;
+            textBox2.Location = new Point(452, 111);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(164, 23);
+            textBox2.TabIndex = 8;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(1162, 430);
+            button3.Name = "button3";
+            button3.Size = new Size(60, 23);
+            button3.TabIndex = 9;
+            button3.Text = "Add";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(1228, 430);
+            button4.Name = "button4";
+            button4.Size = new Size(60, 23);
+            button4.TabIndex = 10;
+            button4.Text = "Modify";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(1294, 430);
+            button5.Name = "button5";
+            button5.Size = new Size(60, 23);
+            button5.TabIndex = 11;
+            button5.Text = "Delete";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(556, 430);
+            button6.Name = "button6";
+            button6.Size = new Size(60, 23);
+            button6.TabIndex = 12;
+            button6.Text = "Delete";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(490, 430);
+            button7.Name = "button7";
+            button7.Size = new Size(60, 23);
+            button7.TabIndex = 13;
+            button7.Text = "Modify";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(424, 430);
+            button8.Name = "button8";
+            button8.Size = new Size(60, 23);
+            button8.TabIndex = 14;
+            button8.Text = "Add";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(1278, 548);
+            button9.Name = "button9";
+            button9.Size = new Size(76, 30);
+            button9.TabIndex = 15;
+            button9.Text = "Exit";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1415, 604);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(textBox2);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(dataGridView2);
+            Controls.Add(dataGridView1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private DataGridView dataGridView2;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
+        private TextBox textBox2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+    }
+}
